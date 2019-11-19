48311848c3e48b2966185c6cc085d68b0d18348d
wayj700 <wayj700@gmail.com>
2019-11-19 09:16:51 -0800

    Pre Alpha Build - Moving Plaltform + Bug Fixes and Improvements
    + Added moving patfrom. You can Change speed and the points it travels to. Has a toggle function for turning on and off. Might need a on button down and up functions.
    
    + Changed how forcegrab distance is calculated. It is now relative to hand's distance from body. Needs to be changed to relative to shoulder position. It would be more natural.

---
+++ Vr Testing/Assets/Scripts/Platform/MovingPlatform.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public GameObject platform;
+    public Transform[] points;
+
+    [SerializeField] private bool isActive = false;
+    [SerializeField] private bool isMoving = false;
+    [SerializeField] private int positionInPoints = 0;
+    private Vector3 target;
+    private Vector3 heading;
+    private float distance;
+
+    public float waitTime = 3.0f;
+    public float lerpSpeed = 10.0f;
+    public float MaxSpeed = 8.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = points[0].position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            heading = target - platform.transform.position;
+            distance = Vector3.Distance(target, platform.transform.position);
+            platform.GetComponent<Rigidbody>().MovePosition(platform.transform.position + (MaxSpeed * (heading / Mathf.Clamp(distance, 1, distance - 1) * Time.deltaTime)));
+        }
+    }
+
+    public void toggleMove()
+    {
+        if (isMoving)
+        {
+            isActive = false;
+            target = platform.transform.position;
+            StopCoroutine("movePlatform");
+        }
+        else
+        {
+            isActive = true;
+            StartCoroutine("movePlatform");
+        }
+    }
+
+    IEnumerator movePlatform()
+    {
+        if (points.Length >= 2)
+        {
+            int direction = 1;
+            int start = 0;
+            int stop = points.Length;
+            int i = positionInPoints;
+
+            while (i != stop)
+            {
+                target = points[i].position;
+                isMoving = true;
+                yield return new WaitUntil(atTarget);
+                isMoving = false;
+                if (direction == 1)
+                {
+                    if (i == stop - 1)
+                    {
+                        start = points.Length;
+                        stop = -1;
+                        direction = -1;
+                        Debug.Log("Reached top of loop");
+                    }
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        start = 0;
+                        stop = points.Length;
+                        direction = 1;
+                        Debug.Log("Reached top of loop");
+                    }
+                }
+                yield return new WaitForSeconds(waitTime);
+                i += direction;
+                positionInPoints = i;
+            }
+        }
+    }
+
+    public bool atTarget()
+    {
+        if (Vector3.Distance(platform.transform.position, target) <= 0.1f)
+        {
+            Debug.Log("at target");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (points.Length >= 2)
+        {
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawLine(points[i].position, points[i + 1].position);
+            }
+        }
+    }
+}
---
+++ Vr Testing/Assets/Scripts/Player Scripts/ForceGrab.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class ForceGrab : MonoBehaviour
+{
+    public GameObject oppositeHand;
+    public float forceMaxSpeed = 5.0f;
+    public float minDistance = 1.0f;
+    public float maxDistance = 25.0f;
+    public float dragAmount = 20.0f;
+    public float lerpSpeed = 0.5f;
+    public float ModSpeed = 0.1f;
+
+    private float currentHoldDist;
+    private Vector3 currentVelocity;
+    private bool isForceGrabbing = false;
+
+    public SteamVR_Action_Boolean Grab;
+    public SteamVR_Action_Boolean Pull;
+    public SteamVR_Action_Boolean Push;
+
+    public Transform raySource;
+    public Transform headPos;
+    private Vector3 ForceHoldPos;
+    public GameObject grabbedObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ForceRay = new Ray(raySource.position, ((transform.up * -1.0f) + transform.forward));
+        Debug.DrawRay(raySource.position, ((transform.up * -1.0f) + transform.forward) * maxDistance, Color.blue);
+        if (this.Grab.state && !isForceGrabbing && Physics.Raycast(raySource.position,((transform.up * -1.0f) + transform.forward), out hit, maxDistance))
+        {
+            //collects object
+            if (hit.collider.tag == "Interactible")
+            {
+                if (Grab.state)
+                {
+                    Debug.Log("Force Grabbing Object");
+                    grabbedObject = hit.collider.gameObject;
+                    hit.rigidbody.useGravity = false;
+                    hit.rigidbody.drag = dragAmount;
+                    currentHoldDist = Vector3.Distance(raySource.position, hit.collider.transform.position);
+                    isForceGrabbing = true;
+                }
+            }
+        }
+        else if (this.isForceGrabbing && this.Grab.state)
+        {
+            //calculates pushing and pulling of object relative to the hand's disntance from head
+            currentHoldDist = Vector3.Distance(new Vector3(headPos.position.x, 0, headPos.position.z), new Vector3(this.transform.position.x, 0, this.transform.position.z)) * ModSpeed;
+
+            //move object towards target position
+            Vector3 heading = ForceRay.GetPoint(currentHoldDist) - grabbedObject.GetComponent<Rigidbody>().position;
+            float distance = Vector3.Distance(ForceRay.GetPoint(Mathf.Clamp(currentHoldDist, minDistance, maxDistance)), grabbedObject.GetComponent<Rigidbody>().position);
+            grabbedObject.GetComponent<Rigidbody>().AddForce(heading * Mathf.Clamp(lerpSpeed * distance * grabbedObject.GetComponent<Rigidbody>().mass, 0, forceMaxSpeed * grabbedObject.GetComponent<Rigidbody>().mass) * Time.deltaTime * 100);
+
+
+            //grabbedObject.GetComponent<Rigidbody>().AddTorque();
+
+
+        }
+        else if (this.isForceGrabbing && !this.Grab.state)
+        {
+            //releases object
+            grabbedObject.GetComponent<Rigidbody>().useGravity = true;
+            grabbedObject.GetComponent<Rigidbody>().drag = 0.0f;
+            grabbedObject = null;
+            this.isForceGrabbing = false;
+        }
+    }
+}
